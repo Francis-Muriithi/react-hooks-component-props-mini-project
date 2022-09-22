e97830eab12639c04a0fd9b3336b4f15f8c981ad
Francis Muriithi <mufraggie@gmail.com>
2022-09-22 13:56:06 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import blogData from "../data/blog";
 import Header from "../components/Header";
 import About from "../components/About";
 import ArticleList from "../components/ArticleList";
 
-console.log(blogData);
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
-      <ArticleList/>
+      <ArticleList id="1" title="Components 101" date= "December 15, 2020" preview="Setting up the building blocks of your site"  minutes= "5"/>
+      <ArticleList id="2" title="React Data Flow" date= "December 1, 2020" preview="Passing props is never passé"  minutes= "15"/>
+      <ArticleList id="3" title="Function vs Class Components" date="January 1, 1970" preview="React, meet OOJS."  minutes= "47"/>
     </div>
   )
 }
