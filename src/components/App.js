4f7863879a1cecee6cef4139dc43c14d4b149de1
Francis Muriithi <mufraggie@gmail.com>
2022-09-22 14:37:11 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,42 @@ import About from "../components/About";
 import ArticleList from "../components/ArticleList";
 
 
+const posts= [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+      minutes: 5,
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+      minutes: 15,
+    },
+    {
+      id: 3,
+      title: "Function vs Class Components",
+      preview: "React, meet OOJS.",
+      minutes: 47,
+    },
+  ]
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
-      <ArticleList id="1" title="Components 101" date= "December 15, 2020" preview="Setting up the building blocks of your site"  minutes= "5"/>
-      <ArticleList id="2" title="React Data Flow" date= "December 1, 2020" preview="Passing props is never passé"  minutes= "15"/>
-      <ArticleList id="3" title="Function vs Class Components" date="January 1, 1970" preview="React, meet OOJS."  minutes= "47"/>
-    </div>
-  )
-}
+      <h1>My Articles List</h1>
+      {posts.map((post) => {
+          const {title, date, minutes, preview}= post;
+          return(
+            <ArticleList title={title} date={date} minutes={minutes} preview={preview}/>
+          )
+          
+      })}
+      </div>
+)}
 
 export default App;
